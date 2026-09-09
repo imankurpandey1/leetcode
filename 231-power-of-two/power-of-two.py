1629586669f21fb8dfb987cd397681a26707c466
imankurpandey1 <ankurpandey9973@gmail.com>
2026-09-10 01:08:53 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (19.81%) - LeetSync

--- 231-power-of-two/power-of-two.py
+++ 231-power-of-two/power-of-two.py
@@ -3,11 +3,9 @@ class Solution:
         if n<=0:
             return False
         while n%2==0:
-                n//=2
+            n//=2
         if n==1:
             return True
         else:
             return False
-
-
-            
+        
