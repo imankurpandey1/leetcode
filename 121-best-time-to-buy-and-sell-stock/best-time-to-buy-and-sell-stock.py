5f955fdc6a9ccf6c431838fc956387b256b86123
imankurpandey1 <ankurpandey9973@gmail.com>
2026-09-14 16:14:56 +0530

    Time: 75 ms (11.12%) | Memory: 28.7 MB (21.46%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -3,7 +3,7 @@ class Solution:
         ans=0
         min_price=prices[0]
         for i in range(len(prices)):
-            curr_profit=prices[i]-min_price
-            ans=max(curr_profit,ans)
+            cur_prof=prices[i]-min_price
+            ans=max(cur_prof,ans)
             min_price=min(min_price,prices[i])
         return ans
