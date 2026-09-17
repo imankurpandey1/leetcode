b11809fec7b0b4d55f4535b917f189f0a22f2038
imankurpandey1 <ankurpandey9973@gmail.com>
2026-09-18 02:40:58 +0530

    Time: 123 ms (93.84%) | Memory: 25.2 MB (93.04%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -0,0 +1,18 @@
+class Solution:
+    def totalFruit(self, fruits: list[int]) -> int:
+        left=0
+        basket={}
+        for right in range(len(fruits)):
+            fruit=fruits[right]
+            if fruit in basket:
+                basket[fruit]+=1
+            else:
+                basket[fruit]=1
+            if len(basket)>2:
+                left_fruit=fruits[left]
+                basket[left_fruit]-=1
+
+                if basket[left_fruit]==0:
+                    del basket[left_fruit]
+                left+=1
+        return len(fruits)-left
